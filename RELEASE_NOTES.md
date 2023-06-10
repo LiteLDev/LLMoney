d2338c5747c5b73650e93deee851deeac25163c2
ShrBox <shrbox@outlook.com>
2023-06-10 22:20:56 +0800

    refactor: update to 2.14.0

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,1 +1,1 @@
-- Support LiteLoaderBDS 2.12.0(1.19.71.02)
+- Support LiteLoaderBDS 2.14.0
--- src/Settings.h
+++ src/Settings.h
@@ -1,17 +1,20 @@
 #include "Nlohmann/json.hpp"
 
-#define LLMONEY_VERSION ll::Version{ 2,13,0 }
+#define LLMONEY_VERSION ll::Version{ 2,14,0 }
 
-namespace Settings
-{
+namespace Settings {
     extern std::string language;
     extern int def_money;
     extern float pay_tax;
     extern bool enable_ranking;
 
     nlohmann::json globaljson();
+
     void initjson(nlohmann::json json);
+
     void WriteDefaultConfig(const std::string &fileName);
+
     void LoadConfigFromJson(const std::string &fileName);
+
     void reloadJson(const std::string &fileName);
 } // namespace Settings
